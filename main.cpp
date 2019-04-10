4c52b89dea8488a47aa2135810c7c120f33a304a
Pieter-Jan Lavaerts <pieter-jan.lavaerts@student.uhasselt.be>
2019-04-10 13:05:09 +0200

    ja nog wat shit heh

--- main.cpp
+++ main.cpp
@@ -155,9 +155,9 @@ int main()
 
         // create transformations
         glm::mat4 view = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -6.0f));
         glm::mat4 projection = glm::mat4(1.0f);
         projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         //pass transformation matrices to the shader
         ourShader.setMat4("projection", projection);
         ourShader.setMat4("view", view);
@@ -166,11 +166,11 @@ int main()
         glBindVertexArray(VAO);
         for (unsigned int i = 0; i < 10; i++)
         {
+            ourShader.setMat4("view", view);
             //calculate the model matrix for each object and pass it to the shader before drawing
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
-            model = glm::translate(model, glm::vec3(0.0f, 0.0f, -3.0f));
-            float angle = 30.0f * (i+1) * glfwGetTime();
+            float angle = 30.0f * (i+1) * (glfwGetTime()+0.5*glm::tan(glfwGetTime()+i));
             model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
             ourShader.setMat4("model", model);
 
