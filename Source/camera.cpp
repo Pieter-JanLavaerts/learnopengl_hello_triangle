cc6f4bcf7826db41572de006732caaa51b932141
Pieter-Jan Lavaerts <pieter-jan.lavaerts@student.uhasselt.be>
2019-04-24 12:43:18 +0200

    nog een testje om te zien of git credentialhelper werkt

--- Source/camera.cpp
+++ Source/camera.cpp
@@ -1,5 +1,6 @@
 #include "camera.h"
 
+
 Camera::Camera(glm::vec3 position, glm::vec3 up, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
 {
     Position = position;
