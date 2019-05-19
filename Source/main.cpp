ee68fe1668db7e0d9cfa92dc9129d6d879d990a4
BertSwerts <bert.swerts@student.uhasselt.be>
2019-05-19 17:50:36 +0200

    Added flashlight and models for planet and asteroids

--- Source/main.cpp
+++ Source/main.cpp
@@ -36,6 +36,35 @@ float lastFrame = 0.0f;
 glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 //Display list
+GLuint renderBoxesDisplay;
+
+/* Render boxes */
+void renderBoxes(glm::vec3 cubePositions[], Shader lightingShader)
+{
+    for (unsigned int i = 0; i < 10; i++)
+    {
+        //calculate the model matrix for each object and pass it to the shader before drawing
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, cubePositions[i]);
+        float angle = 30.0f * (i+1) * (glfwGetTime()+0.5*glm::tan(glfwGetTime()+i));
+        //model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+        lightingShader.setMat4("model", model);
+
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
+}
+
+/* Create display list to render boxes and initialize state*/
+static void init(glm::vec3 cubePositions[], Shader lightingShader)
+{
+    renderBoxesDisplay = glGenLists(1);
+    glNewList(renderBoxesDisplay, GL_COMPILE);
+    renderBoxes(cubePositions, lightingShader);
+    glEndList();
+
+    glShadeModel(GL_FLAT);
+}
+
 int main()
 {
     // glfw: initialize and configure
@@ -76,6 +105,7 @@ int main()
     //Shader lightingShader("Shaders/lighting.vs", "Shaders/lighting.fs");
     Shader lightingShader("../Shaders/lighting.vs", "../Shaders/lighting.fs");
     Shader lampShader("../Shaders/lamp.vs", "../Shaders/lamp.fs");
+
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
@@ -135,7 +165,13 @@ int main()
             glm::vec3( 1.3f, -2.0f, -2.5f),
             glm::vec3( 1.5f,  2.0f, -2.5f),
             glm::vec3( 1.5f,  0.2f, -1.5f),
-            glm::vec3(-1.3f,  1.0f, -1.5f)
+            glm::vec3(-1.3f,  1.0f, -1.5f),
+            glm::vec3(-1.3f,  1.0f, 10.5f)
+    };
+
+    // positions of the point lights
+    glm::vec3 pointLightPositions[] = {
+            glm::vec3(1.2f, 1.0f, 2.0f)
     };
 
     //cube's vao and vbo
@@ -170,8 +206,8 @@ int main()
     Texture specularMap("../Textures/container_specular.jpg");
 
     lightingShader.use();
-    lightingShader.setInt("matetrial.diffuse", 0);
-    lightingShader.setInt("matetrial.specular", 1);
+    lightingShader.setInt("material.diffuse", 0);
+    lightingShader.setInt("material.specular", 1);
 
     init(cubePositions, lightingShader);
     cout << renderBoxesDisplay;
@@ -197,16 +233,35 @@ int main()
 
         // activate shadre when setting uniforms/drawing objects
         lightingShader.use();
-        lightingShader.setVec3("light.position", lightPos);
         lightingShader.setVec3("viewPos", camera.Position);
+        lightingShader.setFloat("material.shininess", 32.0f);
 
+
+        // directional light
+        lightingShader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
+        lightingShader.setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
+        lightingShader.setVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
+        lightingShader.setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
         // light properties
-        lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-        lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
-        lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+        lightingShader.setVec3("pointLights[0].position", pointLightPositions[0]);
+        lightingShader.setVec3("pointLights[0].ambient", 0.05f, 0.05f, 0.05f);
+        lightingShader.setVec3("pointLights[0].diffuse", 0.8f, 0.8f, 0.8f);
+        lightingShader.setVec3("pointLights[0].specular", 1.0f, 1.0f, 1.0f);
+        lightingShader.setFloat("pointLights[0].constant", 1.0f);
+        lightingShader.setFloat("pointLights[0].linear", 0.09);
+        lightingShader.setFloat("pointLights[0].quadratic", 0.032);
+        // spotLight
+        lightingShader.setVec3("spotLight.position", camera.Position);
+        lightingShader.setVec3("spotLight.direction", camera.Front);
+        lightingShader.setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
+        lightingShader.setVec3("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
+        lightingShader.setVec3("spotLight.specular", 1.0f, 1.0f, 1.0f);
+        lightingShader.setFloat("spotLight.constant", 1.0f);
+        lightingShader.setFloat("spotLight.linear", 0.09);
+        lightingShader.setFloat("spotLight.quadratic", 0.032);
+        lightingShader.setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
+        lightingShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(15.0f)));
 
-        // material properties
-        lightingShader.setFloat("material.shininess", 64.0f);
 
         // view/projection transformations
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
@@ -237,7 +292,7 @@ int main()
         lampShader.setMat4("projection", projection);
         lampShader.setMat4("view", camera.GetViewMatrix());
         model = glm::mat4(1.0f);
-        model = glm::translate(model, lightPos);
+        model = glm::translate(model, pointLightPositions[0]);
         model = glm::scale(model, glm::vec3(0.2f)); //smaller cube
         lampShader.setMat4("model", model);
 
@@ -297,6 +352,143 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
         firstMouse = false;
     }
 
+//    // picking
+//    GLuint selectBuf[512];
+//    glSelectBuffer ( 512, selectBuf );
+//    glRenderMode( GL_SELECT );
+//    glInitNames( );
+//
+//    GLint viewport[4];
+//    glGetIntegerv( GL_VIEWPORT, viewport );
+//    // remember current matrix
+//    GLdouble pm[16];
+//    glGetDoublev( GL_PROJECTION_MATRIX, pm );
+//
+//    glMatrixMode( GL_PROJECTION );
+//    glPushMatrix( ); // save current matrix
+//    glLoadIdentity( ); // start with identity
+//    gluPickMatrix( lastX, (viewport[3] - lastY), 5.0, 5.0, viewport ); // create picking region
+//    glMultMatrixd( pm ); // apply projection
+//
+//
+//    float vertices[] = {
+//            -0.5f, -0.5f, 0.0f,
+//            0.5f, -0.5f, 0.0f,
+//            0.0f,  0.5f, 0.0f
+//    };
+//    unsigned int VBO;
+//    glGenBuffers(2, &VBO);
+//    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+//    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+//
+//
+//
+//    // build and compile our shader program
+//    // ------------------------------------
+//    // vertex shader
+//    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+//    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+//    glCompileShader(vertexShader);
+//    // check for shader compile errors
+//    int success;
+//    char infoLog[512];
+//    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+//    if (!success)
+//    {
+//        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+//        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+//    }
+//    // fragment shader
+//    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+//    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+//    glCompileShader(fragmentShader);
+//    // check for shader compile errors
+//    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+//    if (!success)
+//    {
+//        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+//        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+//    }
+//    // link shaders
+//    int shaderProgram = glCreateProgram();
+//    glAttachShader(shaderProgram, vertexShader);
+//    glAttachShader(shaderProgram, fragmentShader);
+//    glLinkProgram(shaderProgram);
+//    // check for linking errors
+//    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+//    if (!success) {
+//        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+//        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+//    }
+//    glDeleteShader(vertexShader);
+//    glDeleteShader(fragmentShader);
+//
+//    // 0. copy our vertices array in a buffer for OpenGL to use
+//    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+//    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+//// 1. then set the vertex attributes pointers
+//    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+//    glEnableVertexAttribArray(0);
+//// 2. use our shader program when we want to render an object
+//    glUseProgram(shaderProgram);
+//// 3. now draw the object
+//
+//
+//    unsigned int VAO;
+//    glGenVertexArrays(1, &VAO);
+//
+//    // ..:: Initialization code (done once (unless your object frequently changes)) :: ..
+//// 1. bind Vertex Array Object
+//    glBindVertexArray(VAO);
+//// 2. copy our vertices array in a buffer for OpenGL to use
+//    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+//    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+//// 3. then set our vertex attributes pointers
+//    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+//    glEnableVertexAttribArray(0);
+//
+//
+//
+//// ..:: Drawing code (in render loop) :: ..
+//// 4. draw the object
+//    glUseProgram(shaderProgram);
+//    glBindVertexArray(VAO);
+//
+//    glUseProgram(shaderProgram);
+//    glBindVertexArray(VAO);
+//    glDrawArrays(GL_TRIANGLES, 0, 3);
+//
+//
+//
+//    glMatrixMode( GL_PROJECTION );
+//    glPopMatrix( );
+//    GLint hits = glRenderMode( GL_RENDER );
+//    cout << hits;
+//    cout << "\n";
+//
+//    GLuint minMinZ = 0xffffffff;
+//    // process hits
+//    int firstHit = -1;
+//    GLuint *ptr = (GLuint *)selectBuf;
+//    for (GLint i = 0; i < hits; i++)
+//    {
+//        GLuint nrNames = *ptr;
+//        ptr++; // skip nrNames
+//        GLuint minZ = *ptr;
+//        ptr++; // skip minZ
+//        GLuint maxZ = *ptr;
+//        ptr++; // skip maxZ
+//        for (GLuint j = 0; j < nrNames; j++)
+//        {
+//            if (minZ < minMinZ)
+//            {
+//                minMinZ = minZ;
+//                firstHit = *ptr;
+//            }
+//            ptr++;
+//        }
+//    }
+
     float xoffset = xpos - lastX;
     float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
 
@@ -314,3 +506,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
+
+
+
