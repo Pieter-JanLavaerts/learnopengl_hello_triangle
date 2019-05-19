28faaa2694d0debef04e5d445fe382fffee16d62
Pieter-JanLavaerts <Pieter-JanLavaerts@student.uhasselt.be>
2019-05-19 20:31:32 +0200

    Added sphere class

---
+++ Headers/Sphere.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <vector>
+
+#include <glm/glm.hpp>
+#include "shader.h"
+
+#define space 10
+#define VERTEX_COUNT (180/space)*(360/space)*2 
+
+const double PI=3.1415926535897;
+
+class Sphere
+{
+public:
+	Sphere();
+	void Draw(glm::mat4 projection, glm::mat4 view, glm::mat4 model);
+
+private:
+    Shader shader = Shader("../Shaders/lamp.vs", "../Shaders/lamp.fs");
+	const int Stacks = 10;
+	const int Slices = 10;
+	const float Radius = 10;
+	std::vector<glm::vec3> vertices;
+	std::vector<int> indices;
+};
---
+++ Source/Sphere.cpp
@@ -0,0 +1,68 @@
+#include "../Headers/Sphere.h"
+
+Sphere::Sphere()
+{
+	// Calc The Vertices
+	for (int i = 0; i <= Stacks; ++i){
+		float V   = i / (float) Stacks;
+		float phi = V * glm::pi <float> ();
+
+		// Loop Through Slices
+		for (int j = 0; j <= Slices; ++j){
+
+			float U = j / (float) Slices;
+			float theta = U * (glm::pi <float> () * 2);
+
+			// Calc The Vertex Positions
+			float x = cosf (theta) * sinf (phi);
+			float y = cosf (phi);
+			float z = sinf (theta) * sinf (phi);
+
+			// Push Back Vertex Data
+			vertices.push_back (glm::vec3(x, y, z) * Radius);
+		}
+	}
+
+	// Calc The Index Positions
+	for (int i = 0; i < Slices * Stacks + Slices; ++i){
+		indices.push_back (i);
+		indices.push_back (i + Slices + 1);
+		indices.push_back (i + Slices);
+
+		indices.push_back (i + Slices + 1);
+		indices.push_back (i);
+		indices.push_back (i + 1);
+	}
+}
+
+void Sphere::Draw(glm::mat4 model, glm::mat4 projection, glm::mat4 view)
+{
+
+	//vao
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	//vbo
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3) * vertices.size(), &vertices[0].x, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	//ibo
+	unsigned int IBO;
+	glGenBuffers(1, &IBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+	//setting up the shader
+	shader.use();
+	shader.setMat4("projection", projection);
+	shader.setMat4("view",  view);
+	shader.setMat4("model", model);
+
+	glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+}
