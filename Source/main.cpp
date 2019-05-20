db2f3a68d4483462fb509861abb815375c25f671
BertSwerts <bert.swerts@student.uhasselt.be>
2019-05-20 04:21:36 +0200

    Added sphere

--- Source/main.cpp
+++ Source/main.cpp
@@ -20,6 +20,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void processInput(GLFWwindow *window);
 unsigned int loadTexture(const char *path);
+void renderSphere();
 
 
 // settings
@@ -53,7 +54,7 @@ void renderBoxes(glm::vec3 cubePositions[], Shader lightingShader)
         //model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
         lightingShader.setMat4("model", model);
 
-        //glDrawArrays(GL_TRIANGLES, 0, 36);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
     }
 }
 
@@ -276,7 +277,7 @@ int main()
     init(cubePositions, lightingShader);
     cout << renderBoxesDisplay;
 
-	Sphere sphere = Sphere();
+//	Sphere sphere = Sphere();
 
     // render loop
     // -----------
@@ -317,6 +318,7 @@ int main()
         lightingShader.setFloat("pointLights[0].linear", 0.09);
         lightingShader.setFloat("pointLights[0].quadratic", 0.032);
         // spotLight
+//        lightingShader.setVec3("spotLight.position", glm::vec3{100.0f,100.0f,100.0f});
         lightingShader.setVec3("spotLight.position", camera.Position);
         lightingShader.setVec3("spotLight.direction", camera.Front);
         lightingShader.setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
@@ -351,17 +353,12 @@ int main()
         init(cubePositions, lightingShader);
         glCallList(renderBoxesDisplay);
 
-        //render model
-		model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f));
-		model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));
-		lightingShader.setMat4("model", model);
-		//ourModel.Draw(lightingShader);
 
         // draw planet
         model = glm::translate(model, glm::vec3(0.0f, -3.0f, 0.0f));
         //model = glm::scale(model, glm::vec3(4.0f, 4.0f, 4.0f));
         lightingShader.setMat4("model", model);
-        planet.Draw(lightingShader);
+        //planet.Draw(lightingShader);
 
         // draw static meteorites
         for (unsigned int i = 0; i < amount; i++)
@@ -396,20 +393,33 @@ int main()
             rock.Draw(lightingShader);
         }
 
-
-        // render the lamp
         lampShader.use();
         lampShader.setMat4("projection", projection);
         lampShader.setMat4("view", camera.GetViewMatrix());
+        glm::vec3 newPos = pointLightPositions[0] + glm::vec3(sin(glfwGetTime() * 5.0) * 5.0, 0.0, 0.0);
+        newPos = pointLightPositions[0];
+//        pbrShader.setVec3("lightPositions[" + std::to_string(i) + "]", newPos);
+//        pbrShader.setVec3("lightColors[" + std::to_string(i) + "]", lightColors[i]);
+
         model = glm::mat4(1.0f);
-        model = glm::translate(model, pointLightPositions[0]);
-        model = glm::scale(model, glm::vec3(0.2f)); //smaller cube
+        model = glm::translate(model,  pointLightPositions[0]);
         lampShader.setMat4("model", model);
+        renderSphere();
 
-        glBindVertexArray(lightVAO);
-        //glDrawArrays(GL_TRIANGLES, 0, 36);
 
-		sphere.Draw(projection, view, model);
+        // render the lamp
+//        lampShader.use();
+//        lampShader.setMat4("projection", projection);
+//        lampShader.setMat4("view", camera.GetViewMatrix());
+//        model = glm::mat4(1.0f);
+//        model = glm::translate(model, pointLightPositions[0]);
+//        model = glm::scale(model, glm::vec3(0.2f)); //smaller cube
+//        lampShader.setMat4("model", model);
+//
+//        glBindVertexArray(lightVAO);
+//        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+//		sphere.Draw(projection, view, model);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -653,6 +663,101 @@ unsigned int loadTexture(char const * path)
     }
 
     return textureID;
+}
+
+unsigned int sphereVAO = 0;
+unsigned int indexCount;
+void renderSphere()
+{
+    if (sphereVAO == 0)
+    {
+        glGenVertexArrays(1, &sphereVAO);
+
+        unsigned int vbo, ebo;
+        glGenBuffers(1, &vbo);
+        glGenBuffers(1, &ebo);
+
+        std::vector<glm::vec3> positions;
+        std::vector<glm::vec2> uv;
+        std::vector<glm::vec3> normals;
+        std::vector<unsigned int> indices;
+
+        const unsigned int X_SEGMENTS = 64;
+        const unsigned int Y_SEGMENTS = 64;
+        const float PI = 3.14159265359;
+        for (unsigned int y = 0; y <= Y_SEGMENTS; ++y)
+        {
+            for (unsigned int x = 0; x <= X_SEGMENTS; ++x)
+            {
+                float xSegment = (float)x / (float)X_SEGMENTS;
+                float ySegment = (float)y / (float)Y_SEGMENTS;
+                float xPos = std::cos(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
+                float yPos = std::cos(ySegment * PI);
+                float zPos = std::sin(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
+
+                positions.push_back(glm::vec3(xPos, yPos, zPos));
+                uv.push_back(glm::vec2(xSegment, ySegment));
+                normals.push_back(glm::vec3(xPos, yPos, zPos));
+            }
+        }
+
+        bool oddRow = false;
+        for (int y = 0; y < Y_SEGMENTS; ++y)
+        {
+            if (!oddRow) // even rows: y == 0, y == 2; and so on
+            {
+                for (int x = 0; x <= X_SEGMENTS; ++x)
+                {
+                    indices.push_back(y       * (X_SEGMENTS + 1) + x);
+                    indices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
+                }
+            }
+            else
+            {
+                for (int x = X_SEGMENTS; x >= 0; --x)
+                {
+                    indices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
+                    indices.push_back(y       * (X_SEGMENTS + 1) + x);
+                }
+            }
+            oddRow = !oddRow;
+        }
+        indexCount = indices.size();
+
+        std::vector<float> data;
+        for (int i = 0; i < positions.size(); ++i)
+        {
+            data.push_back(positions[i].x);
+            data.push_back(positions[i].y);
+            data.push_back(positions[i].z);
+            if (uv.size() > 0)
+            {
+                data.push_back(uv[i].x);
+                data.push_back(uv[i].y);
+            }
+            if (normals.size() > 0)
+            {
+                data.push_back(normals[i].x);
+                data.push_back(normals[i].y);
+                data.push_back(normals[i].z);
+            }
+        }
+        glBindVertexArray(sphereVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, data.size() * sizeof(float), &data[0], GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+        float stride = (3 + 2 + 3) * sizeof(float);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, stride, (void*)(5 * sizeof(float)));
+    }
+
+    glBindVertexArray(sphereVAO);
+    glDrawElements(GL_TRIANGLE_STRIP, indexCount, GL_UNSIGNED_INT, 0);
 }
 
 
